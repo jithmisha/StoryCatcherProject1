90a27fd7bc0e636f722ea9be475b0bb9f1558b7a
TameezDeen <80967823+TameezDeen@users.noreply.github.com>
2021-06-30 20:08:30 +0530

    Renamed

--- app/src/main/java/com/storycatcher/storycatcher/SplashScreen.java
+++ app/src/main/java/com/storycatcher/storycatcher/SplashScreen.java
@@ -5,14 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
     private static int SPLASH_SCREEN = 2000;//2seconds
 
@@ -27,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
 
 
 
@@ -47,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler(). postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent= new Intent(MainActivity.this,SignOrRegister.class);
+                Intent intent= new Intent(SplashScreen.this,SignOrRegister.class);
                 startActivity(intent);
                 finish();
             }
