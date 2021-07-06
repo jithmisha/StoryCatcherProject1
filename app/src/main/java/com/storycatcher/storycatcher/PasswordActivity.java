e20a38b169e17c9bb4762c289500a91e48d2417f
TameezDeen <80967823+TameezDeen@users.noreply.github.com>
2021-07-06 19:54:20 +0530

    password reset

--- app/src/main/java/com/storycatcher/storycatcher/PasswordActivity.java
+++ app/src/main/java/com/storycatcher/storycatcher/PasswordActivity.java
@@ -1,32 +1,41 @@
 package com.storycatcher.storycatcher;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PasswordActivity extends AppCompatActivity {
     private EditText emailPassword;
     private Button resetPassword;
     private ImageButton imgButtonBack;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView(R.layout.activity_password);
 
-        emailPassword=(EditText)findViewById(R.id.txtemailpass);
+        emailPassword=(EditText)findViewById(R.id.recoverAccountEmail);
         resetPassword=(Button)findViewById(R.id.btnresetpassword);
+        auth=FirebaseAuth.getInstance();
 
         resetPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String useremail=emailPassword.getText().toString().trim();
+                resetPassword();
             }
         });
 
@@ -37,8 +46,37 @@ public class PasswordActivity extends AppCompatActivity {
                 openLoginAdapterActivity();
             }
         });
+        }
+
+    private void resetPassword(){
+        String email=emailPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            emailPassword.setError("Email is required");
+            emailPassword.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailPassword.setError("Please provide valid email");
+            emailPassword.requestFocus();
+            return;
+        }
+
+        auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(PasswordActivity.this,"Please check email to reset password",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(PasswordActivity.this,"Try again! Something went wrong",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
     }
+
+    //back button
     public void openLoginAdapterActivity(){
         Intent intent=new Intent(this,SignOrRegister.class);
         startActivity(intent);
