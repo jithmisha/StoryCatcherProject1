79058ffdb45b86f8080b47d1f78ae9be5d3ac50f
TameezDeen <80967823+TameezDeen@users.noreply.github.com>
2021-08-27 17:59:22 +0530

    change

--- app/src/main/java/com/storycatcher/storycatcher/Settings.java
+++ app/src/main/java/com/storycatcher/storycatcher/Settings.java
@@ -3,6 +3,7 @@ package com.storycatcher.storycatcher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +23,8 @@ public class Settings extends AppCompatActivity {
         aboutBtn=findViewById(R.id.settingaboutbutton);
         logoutBtn=findViewById(R.id.settingslogoutbutton);
         backButton=findViewById(R.id.imgBackBtnSettings);
+        final MediaPlayer mp=MediaPlayer.create(this,R.raw.goat);
+
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +33,12 @@ public class Settings extends AppCompatActivity {
             }
         });
 
+        aboutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
         logoutBtn.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
